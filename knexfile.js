7c72b671795c887e8f5b7483d24e0eb49ed8a22e
Tristannica <gilford.tristan@gmail.com>
2017-06-05 14:12:22 -0600

    modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,12 +8,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-     database: process.env.DB,
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     port: process.env.DB_PORT,
-     host: process.env.DB_HOST
-     }
-   }
+    connection: process.env.DATABASE_URL
+}
+
 }
