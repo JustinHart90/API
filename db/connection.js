c2b8271bf6f6e9588d9204827cc662122bc92259
Tristannica <gilford.tristan@gmail.com>
2017-06-06 15:39:58 -0600

    nada

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,4 @@
+const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
 
-const environment = process.env.NODE_ENV || 'development';
-
-const connection = config[environment]
-
-const pg = require('knex')(connection)
-
-module.exports = pg
+module.exports = require('knex')(config);
