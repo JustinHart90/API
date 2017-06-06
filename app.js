e45b985ab74284b74ac07eb24faf92072e123a19
Tristannica <gilford.tristan@gmail.com>
2017-06-06 13:35:51 -0600

    enabled CORS on server

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const micro = require('./routes/micro');
 
 const app = express();
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,6 +23,7 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use()
 
 app.use('/api/micro', micro);
 
