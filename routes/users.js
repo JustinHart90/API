d874e858ffb92d8a2d073298955282ca301a9102
devin hanaway <devinh11@gmail.com>
2017-06-08 22:24:45 -0600

    dynamically adding profile info

--- routes/users.js
+++ routes/users.js
@@ -20,18 +20,18 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   console.log(req.params.id);
-  // queries.getUser(req.params.id)
-  // .then((user) => {
-  //   res.json({
-  //     status: 'success',
-  //     data: user
-  //   });
-  // })
-  // .catch((err) => {
-  //     console.log('They see me routing dirty!');
-  //     return next(err);
-  //    //res.json(err);
-  // });
+  queries.getUser(req.params.id)
+  .then((user) => {
+    res.json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+      console.log('They see me routing dirty!');
+      return next(err);
+     //res.json(err);
+  });
 });
 
 
