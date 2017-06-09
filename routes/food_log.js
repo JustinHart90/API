d55577cf8fde5b10ea0f8d988f975b91a4dfa815
devin hanaway <devinh11@gmail.com>
2017-06-09 03:42:03 -0600

    pushing live code too late

--- routes/food_log.js
+++ routes/food_log.js
@@ -46,7 +46,7 @@ router.post('/:id', (req, res, next) => {
   return queries.postFoodItem(id, macros, micros, req.body.name, req.body.quantity, req.body.measurement)
     .then(food_id => {
       let foodId = food_id[0]
-      micros["food_id"] = foodId
+      micros["food_id"] = foodId[0]
       console.log(micros);
       queries.postMicros(micros)
       return food_id
