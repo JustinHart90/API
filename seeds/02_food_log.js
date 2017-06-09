53f59268ac1cd319e97c44d55d8fd77ecdbbe1ae
devin hanaway <devinh11@gmail.com>
2017-06-08 19:49:22 -0600

    updated migrations

--- seeds/02_food_log.js
+++ seeds/02_food_log.js
@@ -5,13 +5,13 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('food_log').insert([
         {
-            user_id: '1',
+            user_id: '10213378191999196',
             food_name: 'chicken',
             quantity: '3',
             size: 'medium',
         },
         {
-            user_id: '2',
+            user_id: '10213378191999196',
             food_name: 'spinach',
             quantity: '1',
             size: 'large',
