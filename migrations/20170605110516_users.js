6030246060aa5e19b42be43028872b00a4d932e5
devin hanaway <devinh11@gmail.com>
2017-06-08 19:35:12 -0600

    updated migrations

--- migrations/20170605110516_users.js
+++ migrations/20170605110516_users.js
@@ -1,14 +1,14 @@
 exports.up = (knex) => {
   return knex.schema.createTable('users', (table) => {
-    table.increments('id');
+    table.string('id').notNullable();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
-    table.string('username').notNullable();
-    table.string('password').notNullable();
+    table.string('username');
+    table.string('password');
     table.boolean('is_admin').notNullable().defaultTo(false);
-    table.integer('age').notNullable();
-    table.string('sex').notNullable();
-    table.string('weight').notNullable();
+    table.integer('age');
+    table.string('sex');
+    table.string('weight');
     table.timestamp('created_on').defaultTo(knex.fn.now())
     table.timestamp('updated_on').defaultTo(knex.fn.now())
   });
